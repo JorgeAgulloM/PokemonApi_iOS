84f2505e2a38a8130c8b3b4776478c3d1684ad60
JorgeAgulloM <agullojorge@gmail.com>
2022-02-16 07:05:19 +0100

    Initial Commit

---
+++ PokeAPIStoryBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokeAPIStoryBoard
+//
+//  Created by Jorge Agullo Martin on 16/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PokeAPIStoryBoardUITests/PokeAPIStoryBoardUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PokeAPIStoryBoardUITestsLaunchTests.swift
+//  PokeAPIStoryBoardUITests
+//
+//  Created by Jorge Agullo Martin on 16/2/22.
+//
+
+import XCTest
+
+class PokeAPIStoryBoardUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
